b62d566b929e3068e22f3fd0ff14d985300d88ca
ismayilov1995 <ismayilov1995@gmail.com>
2021-02-17 01:36:23 +0400

    create simple cache for movie detail

--- lib/resources/repository.dart
+++ lib/resources/repository.dart
@@ -4,10 +4,22 @@ import 'package:movie_app/resources/movie_api_provider.dart';
 class Repository {
   final movieApiProvider = MovieApiProvider();
 
+  Map<int, Movie> cachedMovie = Map();
+
   Future<ItemModel> fetchAllMovies({bool isPopular}) =>
       movieApiProvider.fetchMovieList(isPopular: isPopular);
 
   Future<GenresModel> fetchGenreList() => movieApiProvider.fetchGenreList();
 
-  Future<Movie> fetchMovie(int id) => movieApiProvider.fetchMovie(id);
+  Future<Movie> fetchMovie(int id) async {
+    Movie m;
+    if (cachedMovie.containsKey(id)) {
+      m = cachedMovie[id];
+    } else {
+      final mov = await movieApiProvider.fetchMovie(id);
+      cachedMovie[id] = mov;
+      m = mov;
+    }
+    return m;
+  }
 }
